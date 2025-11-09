d8c8f866948643e80d7783f8073594d70a9af933
Daniil Shevchenko <daniil.shevchenko.shi.2023@lpnu.ua>
2025-11-09 22:23:45 +0200

    Added house

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,10 @@
-﻿class Program
+﻿class House
+{
+    public string Street { get; set; }
+    public string Number { get; set; }
+
+}
+class Program
 {
     public static void Main(string[] args)
     {
