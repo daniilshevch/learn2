da89690d87bb5a42e1fb14100eeabb877b682c49
Daniil Shevchenko <daniil.shevchenko.shi.2023@lpnu.ua>
2025-11-09 22:40:30 +0200

    Student

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,11 @@
     public string Name { get; set; }
     public int Age { get; set; }
 }
+class Student
+{
+    public string School { get; set; }
+    public string Name { get; set; }
+}
 class Program
 {
     public static void Main(string[] args)
